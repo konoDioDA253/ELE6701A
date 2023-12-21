a7b7f2b2b17b77aab6042e6e696649838d8d80b6
Bouh <bouh.abdillahi@polymtl.ca>
2023-12-21 05:38:29 -0500

    WIP Partie II, functionning just needs mmse tweaked

--- ELE6701_Projet_PartieII.m
+++ ELE6701_Projet_PartieII.m
@@ -64,41 +64,76 @@ vecteurs_aleatoires_envoyes(vecteurs_aleatoires_envoyes == 2) = -1;
 y=vecteurs_aleatoires_envoyes + vecteurs_bruit_gaussien;
 
 %% Parametres pour calcul egalisteur MMSE
-Rs = eye(10);
+Rs = eye(3);
 Rn = variance_desiree*Rs;
 Delta=[0 1 2];
-eDelta1 = [1;0;0;0;0;0;0;0;0;0];
-eDelta2 = [0;1;0;0;0;0;0;0;0;0];
-eDelta3 = [0;0;1;0;0;0;0;0;0;0];
+
+H=[1 -0.4 0 0;
+    0 1 -0.4 0;
+    0 0 1 -0.4];
+eDelta1 = [1;0;0;0];
+eDelta2 = [0;1;0;0];
+eDelta3 = [0;0;1;0];
 Ry = Rs*H*(H') + Rn;
 
+
 %% Egalisateur MMSE
-w1 = Rs*(eDelta1')*(H')*Ry^-1;
-w2 = Rs*(eDelta2')*(H')*Ry^-1;
-w3 = Rs*(eDelta3')*(H')*Ry^-1;
+w1 = (eDelta1')*(H')*Ry^-1;
+w2 = (eDelta2')*(H')*Ry^-1;
+w3 = (eDelta3')*(H')*Ry^-1;
 
 MMSE1 = Rs - w1*Ry*(w1');
 MMSE2 = Rs - w2*Ry*(w2');
 MMSE3 = Rs - w3*Ry*(w3');
 
 %% Decision MMSE
+% s_chapeau_iDelta1 =  zeros(10,nombre_paquets);
+s_chapeau_iDelta2 =  zeros(10,nombre_paquets);
+% s_chapeau_iDelta3 =  zeros(10,nombre_paquets);
 
-
-%% Detection ML
-comparison_matrix=-1000000*ones(1,1024);
-indice_guess = -1*ones(1,nombre_paquets);
-vecteurs_s_guess = -3434314324*ones(10,nombre_paquets);
-error_count_vector = 0;
+% vecteur_guess_s_Delta1 = zeros(10,nombre_paquets);
+vecteur_guess_s_Delta2 = zeros(10,nombre_paquets);
+% vecteur_guess_s_Delta3 = zeros(10,nombre_paquets);
 error_count_symbol = 0;
+error_count_vector = 0;
 for i=1:1:nombre_paquets
+%     s_chapeau_iDelta1(:,i) = w1*[y(1,i); y(); y()];
     
-    for j=1:1:1024
-        comparison_matrix(1,j) = real(transpose(matrice_vecteurs_hypothese_s(:,j))*y(:,i) - 0.5*transpose(matrice_vecteurs_hypothese_s(:,j))*matrice_vecteurs_hypothese_s(:,j));
+    s_chapeau_iDelta2(:,i) = [w2*[y(2,i); y(1,i); 0];
+                                            w2*[y(3,i); y(2,i); y(1,i)];
+                                             w2*[y(4,i); y(3,i); y(2,i)];
+                                             w2*[y(5,i); y(4,i); y(3,i)];
+                                             w2*[y(6,i); y(5,i); y(4,i)];
+                                             w2*[y(7,i); y(6,i); y(5,i)];
+                                             w2*[y(8,i); y(7,i); y(6,i)];
+                                             w2*[y(9,i); y(8,i); y(7,i)];
+                                             w2*[y(10,i); y(9,i); y(8,i)];
+                                             w2*[0; y(10,i); y(9,i)];
+                                            ];
+    
+%     s_chapeau_iDelta3(:,i) = w3*y(:,i);
+    
+    for j=1:1:10        
+%         if s_chapeau_iDelta1(j,i) < 0
+%             vecteur_guess_s_Delta1(j,i) = -1;
+%         else
+%             vecteur_guess_s_Delta1(j,i) = 1;
+%         end
+%         
+        if s_chapeau_iDelta2(j,i) < 0
+            vecteur_guess_s_Delta2(j,i) = -1;
+        else
+            vecteur_guess_s_Delta2(j,i) = 1;
+        end
+        
+%         if s_chapeau_iDelta3(j,i) < 0
+%             vecteur_guess_s_Delta3(j,i) = -1;
+%         else
+%             vecteur_guess_s_Delta3(j,i) = 1;
+%         end      
     end
-    [max_value, indice_guess(1,i)] = max(comparison_matrix);
-    vecteurs_s_guess(:,i) = matrice_vecteurs_hypothese_s(:,indice_guess(1,i));
-    comp = isequal(vecteurs_aleatoires_envoyes(:,i), vecteurs_s_guess(:,i));
-    difference_per_symbol = nnz(vecteurs_s_guess(:,i) ~= vecteurs_aleatoires_envoyes(:,i));
+    comp = isequal(vecteurs_aleatoires_envoyes(:,i), vecteur_guess_s_Delta2(:,i));
+    difference_per_symbol = nnz(vecteur_guess_s_Delta2(:,i) ~= vecteurs_aleatoires_envoyes(:,i));
     error_count_symbol = error_count_symbol + difference_per_symbol;
     if comp
         test = 0;
@@ -106,12 +141,41 @@ for i=1:1:nombre_paquets
         error_count_vector = error_count_vector+1;
         disp("error!");
     end
-    
-    
 end
-taux_erreur = error_count_vector/nombre_paquets
+
+taux_erreur_par_paquet_de_10_symboles = error_count_vector/nombre_paquets
 taux_erreur_par_symbole = error_count_symbol/(nombre_paquets*10)
 
+
+
+% % %% Detection ML
+% % comparison_matrix=-1000000*ones(1,1024);
+% % indice_guess = -1*ones(1,nombre_paquets);
+% % vecteurs_s_guess = -3434314324*ones(10,nombre_paquets);
+% % error_count_vector = 0;
+% % error_count_symbol = 0;
+% % for i=1:1:nombre_paquets
+% %     
+% %     for j=1:1:1024
+% %         comparison_matrix(1,j) = real(transpose(matrice_vecteurs_hypothese_s(:,j))*y(:,i) - 0.5*transpose(matrice_vecteurs_hypothese_s(:,j))*matrice_vecteurs_hypothese_s(:,j));
+% %     end
+% %     [max_value, indice_guess(1,i)] = max(comparison_matrix);
+% %     vecteurs_s_guess(:,i) = matrice_vecteurs_hypothese_s(:,indice_guess(1,i));
+% %     comp = isequal(vecteurs_aleatoires_envoyes(:,i), vecteurs_s_guess(:,i));
+% %     difference_per_symbol = nnz(vecteurs_s_guess(:,i) ~= vecteurs_aleatoires_envoyes(:,i));
+% %     error_count_symbol = error_count_symbol + difference_per_symbol;
+% %     if comp
+% %         test = 0;
+% %     else
+% %         error_count_vector = error_count_vector+1;
+% %         disp("error!");
+% %     end
+% %     
+% %     
+% % end
+% % taux_erreur = error_count_vector/nombre_paquets
+% % taux_erreur_par_symbole = error_count_symbol/(nombre_paquets*10)
+% % 
 
 
 
